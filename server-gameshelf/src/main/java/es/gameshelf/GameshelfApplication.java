e4dbee492b5cf16a88e1695efae4dc28d51689a4
nalleon <nabil14716@gmail.com>
2025-06-11 15:09:16 +0100

    feat(project): Added java server project

---
+++ server-gameshelf/src/main/java/es/gameshelf/GameshelfApplication.java
@@ -0,0 +1,13 @@
+package es.gameshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshelfApplication.class, args);
+	}
+
+}
