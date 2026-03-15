6335df4f9b95efcb6f7011a1cb701ce94e06c531
nalleon <nabil14716@gmail.com>
2026-03-15 20:03:15 +0000

    mend
    feat(models): Implemented softdelete in models; closes #58

--- gameself-backend/classifications/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassificationsConfig(AppConfig):
-    name = 'classifications'
--- gameself-backend/libraries/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import LibraryItem
-
-@admin.register(LibraryItem)
-class LibraryAdmin(admin.ModelAdmin):
-    pass
-
-
