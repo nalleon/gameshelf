221c965c2b5e6ae44a5b862ee09ff98a1f4e61b7
PeterMartEsc <petermartesc@gmail.com>
2026-03-25 16:49:51 +0000

    refactor(gameshelf-frontend): minor changes and refactor

--- gameshelf-frontend/src/components/Navbar.vue
+++ gameshelf-frontend/src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="sticky top-0 z-50 bg-navbar backdrop-blur border-b border-white/10">
+    <header class="sticky top-0 z-50 backdrop-blur border-b">
         <div class="max-w-4xl mx-auto px-6 py-4 flex items-center justify-between">
             <!-- IZQUIERDA -->
-            <p class="font-semibold text-white/70">
+            <p class="font-semibold">
                 <router-link to="/">Pedro Martín Escuela</router-link>
             </p>
             <!-- DERECHA (ESCRITORIO) -->
-            <nav class="hidden sm:flex gap-6 text-sm text-white/70">
+            <nav class="hidden sm:flex gap-6 text-sm">
                 <span class="hover:text-white">
                     <router-link to="/">Home</router-link>
                 </span>
@@ -17,26 +17,7 @@
                     <router-link to="/profile">Profile</router-link>
                 </span>
             </nav>
-
-            <!-- BOTÓN (MÓVIL) -->
-            <!-- <button
-                class="sm:hidden rounded-lg border border-white/15 px-3 py-2 text-sm text-white/80 hover:text-white hover:border-white/25"
-                @click="isOpen = !isOpen"
-                :aria-expanded="isOpen ? 'true' : 'false'"
-                aria-label="Abrir menú"
-            >
-                ☰
-            </button> -->
         </div>
-
-        <!-- MENÚ DESPLEGABLE (MÓVIL) -->
-        <!-- <div v-if="isOpen" class="sm:hidden border-t border-white/10">
-            <nav class="max-w-4xl mx-auto px-6 py-4 flex flex-col gap-3 text-sm text-white/70">
-                <a href="#sobre-mi" class="hover:text-white" @click="closeMenu">Sobre mí</a>
-                <a href="#proyectos" class="hover:text-white" @click="closeMenu">Proyectos</a>
-                <a href="#contacto" class="hover:text-white" @click="closeMenu">Contacto</a>
-            </nav>
-        </div> -->
     </header>
 </template>
 
