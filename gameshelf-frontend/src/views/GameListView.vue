641b558a2e5176ec974cde943a0d034660526417
PeterMartEsc <petermartesc@gmail.com>
2026-04-07 22:21:26 +0100

    feat: working on gameList. #80

--- gameshelf-frontend/src/views/GameListView.vue
+++ gameshelf-frontend/src/views/GameListView.vue
@@ -2,15 +2,40 @@
     <Navbar/>
     <div class="mt-10 grid grid-cols-5 grid-rows-3 gap-4 gap-y-20 justify-items-center">
         <div class="bg-gray-50 w-50 h-80" v-for="game in games">
-            {{ game }}
+            <p class="text-gs-claro">{{ game.title }}</p>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
+import { onMounted, ref } from 'vue';
+import type { Game } from '@/types/generalTypes';
 
-const games = ['Juan', 'Pedro', 'Ana', 'game 4', 'game 5', 'game 6']
+
+let games = ref<Game[]>([])
+
+onMounted(async () => {
+    try {
+        const webhookUrl = 'http://127.0.0.1:8000/api/games/'
+
+        const response = await fetch(webhookUrl, 
+            {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }    
+            }
+        );
+
+        const data = await response.json();
+        games.value = data;
+        console.log(data)
+    } catch (err) {
+        console.error(err);
+    }
+});
+
 
 </script>
 
