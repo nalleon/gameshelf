19c3153b27aa6d9c89121532ab397b56a0bd0c2b
nalleon <nabil14716@gmail.com>
2026-05-01 20:45:34 +0100

    test(games): Implemented unit tests for every games app model

--- gameshelf-backend/tests/factories/games.py
+++ gameshelf-backend/tests/factories/games.py
@@ -1,26 +1,56 @@
 import factory
-from factory import fuzzy
+from django.contrib.auth import get_user_model
+from faker import Faker
 
 from games.models import FavoriteItem, Game, Media, Review
+from tests.factories.classifications import (
+    DeveloperFactory,
+    GenreFactory,
+    PlatformFactory,
+    PublisherFactory,
+    RegionFactory,
+)
+
+fake = Faker()
+User = get_user_model()
 
 
+# -------------------------
+# USER
+# -------------------------
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'user{n}')
+    email = factory.LazyAttribute(lambda o: f'{o.username}@test.com')
+    password = factory.PostGenerationMethodCall('set_password', 'password123')
+
+
+# -------------------------
+# GAME
+# -------------------------
 class GameFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Game
-        django_get_or_create = ('title', 'released_at', 'region')
 
-    igdb_id = factory.Sequence(lambda n: 1000 + n)
-    title = factory.Faker('sentence', nb_words=3)
-    description = factory.Faker('paragraph')
-    cover_default = factory.Faker('image_url')
-    cover_detail = factory.Faker('image_url')
-    released_at = factory.Faker('date_object')
+    igdb_id = factory.Sequence(lambda n: n + 10000)
+    title = factory.Sequence(lambda n: f'Game {n}')
+    description = factory.LazyFunction(lambda: fake.text(max_nb_chars=200))
+
+    cover_default = factory.LazyFunction(lambda: fake.image_url())
+    cover_detail = factory.LazyFunction(lambda: fake.image_url())
+
+    released_at = factory.LazyFunction(
+        lambda: fake.date_between(start_date='-10y', end_date='today')
+    )
 
     parent_game_igdb = None
-    region = factory.SubFactory('apps.classifications.factories.RegionFactory')
 
-    age_rating = fuzzy.FuzzyChoice(['E', 'T', 'M', 'PEGI 12', 'PEGI 18'])
-    mature_content = factory.Faker('boolean')
+    region = factory.SubFactory(RegionFactory)
+
+    age_rating = 'TBA'
+    mature_content = True
 
     @factory.post_generation
     def platforms(self, create, extracted, **kwargs):
@@ -29,6 +59,8 @@ class GameFactory(factory.django.DjangoModelFactory):
         if extracted:
             for platform in extracted:
                 self.platforms.add(platform)
+        else:
+            self.platforms.add(PlatformFactory())
 
     @factory.post_generation
     def genres(self, create, extracted, **kwargs):
@@ -37,46 +69,65 @@ class GameFactory(factory.django.DjangoModelFactory):
         if extracted:
             for genre in extracted:
                 self.genres.add(genre)
+        else:
+            self.genres.add(GenreFactory())
 
     @factory.post_generation
     def developers(self, create, extracted, **kwargs):
         if not create:
             return
         if extracted:
-            for developer in extracted:
-                self.developers.add(developer)
+            for dev in extracted:
+                self.developers.add(dev)
+        else:
+            self.developers.add(DeveloperFactory())
 
     @factory.post_generation
     def publishers(self, create, extracted, **kwargs):
         if not create:
             return
         if extracted:
-            for publisher in extracted:
-                self.publishers.add(publisher)
+            for pub in extracted:
+                self.publishers.add(pub)
+        else:
+            self.publishers.add(PublisherFactory())
 
 
+# -------------------------
+# REVIEW
+# -------------------------
 class ReviewFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Review
 
-    content = factory.Faker('text')
-    recommend = factory.Faker('boolean')
+    content = factory.LazyFunction(lambda: fake.text(max_nb_chars=300))
+    recommend = factory.LazyFunction(lambda: fake.boolean())
+
     game = factory.SubFactory(GameFactory)
-    author = factory.SubFactory('apps.users.factories.UserFactory')
+    author = factory.SubFactory(UserFactory)
 
 
+# -------------------------
+# MEDIA
+# -------------------------
 class MediaFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Media
 
     review = factory.SubFactory(ReviewFactory)
+
+    # evita problemas con ImageField en tests
     image = factory.django.ImageField(color='blue')
 
 
+# -------------------------
+# FAVORITE ITEM
+# -------------------------
 class FavoriteItemFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = FavoriteItem
 
     game = factory.SubFactory(GameFactory)
-    user = factory.SubFactory('apps.users.factories.UserFactory')
+    user = factory.SubFactory(UserFactory)
+
     order = factory.Sequence(lambda n: n)
--- gameshelf-backend/tests/test_games.py
+++ gameshelf-backend/tests/test_games.py
@@ -1,76 +1,180 @@
+from datetime import date
+
 import pytest
 from django.db import IntegrityError
-from django.utils.text import slugify
 
-from games.models import FavoriteItem, Game
+from tests.factories.classifications import RegionFactory
+from tests.factories.games import (
+    FavoriteItemFactory,
+    GameFactory,
+    MediaFactory,
+    ReviewFactory,
+    UserFactory,
+)
 
-from tests.factories.games import FavoriteItemFactory, GameFactory, MediaFactory, ReviewFactory
-
-
-# @pytest.mark.django_db
-# class TestGameModel:
-#     def test_game_creation(self):
-#         game = GameFactory(title='Super Mario')
-#         assert game.id is not None
-#         assert 'Super Mario' in str(game)
-#         assert game.slug == slugify('Super Mario')
-
-#     def test_game_slug_uniqueness_with_year(self):
-#         game1 = GameFactory(title='Zelda', released_at='2023-01-01')
-#         game2 = GameFactory(title='Zelda', released_at='2024-01-01')
-
-#         assert game1.slug == 'zelda'
-#         assert game2.slug == 'zelda-2024'
-
-#     def test_game_slug_collision_incremental(self):
-#         game1 = GameFactory(title='Sonic', released_at='2020-01-01')
-#         game2 = Game(title='Sonic', released_at='2020-01-01', igdb_id=99)
-#         game2.save()
-
-#         assert game1.slug == 'sonic'
-#         assert game2.slug == 'sonic-2020-1'
-
-#     def test_game_unique_together_constraint(self):
-#         game = GameFactory()
-#         with pytest.raises(IntegrityError):
-#             Game.objects.create(
-#                 title=game.title, released_at=game.released_at, region=game.region, igdb_id=123
-#             )
+# -------------------------
+# GAME
+# -------------------------
 
 
-# @pytest.mark.django_db
-# class TestReviewModel:
-#     def test_review_creation_and_str(self):
-#         review = ReviewFactory(content='Excelente juego')
-#         assert review.id is not None
-#         assert 'Excelente juego' in str(review)
-#         assert review.game.reviews.count() == 1
+@pytest.mark.django_db
+def test_game_slug_is_generated():
+    game = GameFactory(title='The Last of Us')
+
+    assert game.slug == 'the-last-of-us'
 
 
-# @pytest.mark.django_db
-# class TestMediaModel:
-#     def test_media_creation_and_str(self):
-#         media = MediaFactory()
-#         assert media.id is not None
-#         assert f'Media(id={media.pk}' in str(media)
-#         assert media.review.media_items.count() == 1
+@pytest.mark.django_db
+def test_game_slug_with_same_title_uses_year():
+    region = RegionFactory()
+
+    g1 = GameFactory(title='Halo', released_at=date(2020, 1, 1), region=region)
+    g2 = GameFactory(title='Halo', released_at=date(2021, 1, 1), region=region)
+    assert g1.slug == 'halo'
+    assert g2.slug.startswith('halo-2021')
 
 
-# @pytest.mark.django_db
-# class TestFavoriteItemModel:
-#     def test_favorite_creation_and_str(self):
-#         fav = FavoriteItemFactory()
-#         assert fav.id is not None
-#         assert f'game="{fav.game}"' in str(fav)
+@pytest.mark.django_db
+def test_game_slug_collision_increments():
+    region = RegionFactory()
 
-#     def test_favorite_unique_user_game(self):
-#         fav = FavoriteItemFactory()
-#         with pytest.raises(IntegrityError):
-#             FavoriteItem.objects.create(user=fav.user, game=fav.game)
+    g1 = GameFactory(title='Halo', released_at=date(2020, 1, 1), region=region)
+    g2 = GameFactory(title='Halo', released_at=date(2021, 1, 1), region=region)
 
-#     def test_favorite_ordering(self):
-#         fav1 = FavoriteItemFactory(order=2)
-#         fav2 = FavoriteItemFactory(order=1)
-#         favorites = FavoriteItem.objects.all()
-#         assert favorites[0] == fav2
-#         assert favorites[1] == fav1
+    assert g2.slug == 'halo-2021'
+    assert g2.slug != g1.slug
+
+
+@pytest.mark.django_db
+def test_game_str_contains_fields():
+    game = GameFactory(title='Zelda')
+
+    s = str(game)
+
+    assert str(game.pk) in s
+    assert 'Zelda' in s
+    assert game.slug in s
+
+
+@pytest.mark.django_db
+def test_game_unique_together():
+    region = RegionFactory()
+
+    GameFactory(title='FIFA', released_at=date(2022, 1, 1), region=region)
+
+    with pytest.raises(IntegrityError):
+        GameFactory(title='FIFA', released_at=date(2022, 1, 1), region=region)
+
+
+@pytest.mark.django_db
+def test_game_relationships_are_created():
+    game = GameFactory()
+
+    assert game.platforms.count() > 0
+    assert game.genres.count() > 0
+    assert game.developers.count() > 0
+    assert game.publishers.count() > 0
+
+
+# -------------------------
+# REVIEW
+# -------------------------
+
+
+@pytest.mark.django_db
+def test_review_creation():
+    review = ReviewFactory()
+
+    assert review.pk is not None
+    assert review.game is not None
+    assert review.author is not None
+
+
+@pytest.mark.django_db
+def test_review_str():
+    review = ReviewFactory()
+
+    s = str(review)
+
+    assert str(review.pk) in s
+    assert review.content[:10] in s
+
+
+@pytest.mark.django_db
+def test_review_timestamps():
+    review = ReviewFactory()
+
+    assert review.created_at is not None
+    assert review.updated_at is not None
+
+
+# -------------------------
+# MEDIA
+# -------------------------
+
+
+@pytest.mark.django_db
+def test_media_creation():
+    media = MediaFactory()
+
+    assert media.pk is not None
+    assert media.review is not None
+
+
+@pytest.mark.django_db
+def test_media_str():
+    media = MediaFactory()
+
+    s = str(media)
+
+    assert str(media.pk) in s
+    assert str(media.review.pk) in s
+
+
+# -------------------------
+# FAVORITE
+# -------------------------
+
+
+@pytest.mark.django_db
+def test_favorite_item_creation():
+    fav = FavoriteItemFactory()
+
+    assert fav.pk is not None
+    assert fav.user is not None
+    assert fav.game is not None
+
+
+@pytest.mark.django_db
+def test_favorite_unique_constraint():
+    user = UserFactory()
+    game = GameFactory()
+
+    FavoriteItemFactory(user=user, game=game)
+
+    with pytest.raises(IntegrityError):
+        FavoriteItemFactory(user=user, game=game)
+
+
+@pytest.mark.django_db
+def test_favorite_ordering():
+    user = UserFactory()
+
+    f1 = FavoriteItemFactory(user=user, order=2)
+    f2 = FavoriteItemFactory(user=user, order=1)
+
+    favorites = list(user.favorites.all())
+
+    assert favorites[0].order == 1
+    assert favorites[1].order == 2
+
+
+@pytest.mark.django_db
+def test_favorite_str():
+    fav = FavoriteItemFactory()
+
+    s = str(fav)
+
+    assert str(fav.pk) in s
+    assert str(fav.game.pk) in s
+    assert str(fav.user.pk) in s
