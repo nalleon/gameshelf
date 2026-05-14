aeddfba9f32f88bb71ac5079b6825b7fbb27c4c9
nalleon <nabil14716@gmail.com>
2026-05-14 16:02:44 +0000

    fix(deployment): Fixed missing db volume and deployed on prod machine #138

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,12 @@ set -e
 uv run python manage.py migrate --noinput
 uv run python manage.py collectstatic --noinput --clear
 
+if [ ! -f /app/data/.init_done ]; then
+  just makemigrations
+  just migrate
+  just seed
+  just create-su
+  touch /app/data/.init_done
+fi
+
 exec "$@"
